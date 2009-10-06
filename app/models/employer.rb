bc90a3c263aebb7ee0e0de34cbc69e56fbf5dac6
Markus Hjort <markus.hjort@reaktor.fi>
2009-10-06 18:24:13 +0300

    Implemented validation

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,2 +1,3 @@
 class Employer < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :ssn, :address, :postal_code, :city
 end
--- test/unit/employer_test.rb
+++ test/unit/employer_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class EmployerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    @employer = Employer.new
+    @employer.first_name = 'Ron'
+    @employer.last_name = 'Jeremy'
+    @employer.address = 'Mannerheimintie 2'
+    @employer.ssn = '111111-111C'
+    @employer.postal_code = '00910'
+    @employer.city = 'Helsinki'
+  end
+
+  def accepts_saving_of_valid_employer
+    assert @employer.save
+  end
+
+  def test_does_not_accept_saving_of_employer_without_required_fiels
+    @employer.first_name = ''
+    assert !@employer.save
   end
 end
