bedc7eb5b8de6128f6a8c1a84c55cf01211aee92
Markus Hjort <markus.hjort@reaktor.fi>
2009-10-07 19:02:51 +0300

    Added mock for invoicing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :employers
+  map.resources :mock_invoicing
 
   # The priority is based upon order of creation: first created -> highest priority.
 
