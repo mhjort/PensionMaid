e9cf5aa57456604044d1eae9a3ac55b9d9c525f6
tkiuru <tiina.kiuru@reaktor.fi>
2009-10-13 15:06:54 +0300

    Refactored feedback steps to different file

--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -17,31 +17,15 @@ When /^I order the invoice$/ do
   @browser.wait_for_page_to_load(2000)
 end
 
-Then /^I get a confirmation message$/ do
-  @browser.is_element_present('orderStatus').should be_true
-  confirmation_message = @browser.get_text('orderStatus')
-  @agreement_id = confirmation_message.scan(/\d+/)[0]
-  puts @agreement_id
-end
-
 Then /^the order is registered$/ do
   count = @db.get_first_value("select count(*) from employers where ssn = '#@ssn'" )
   count.to_i.should eql 1
-end
-
-Then /^I get an error message$/ do
-  @browser.is_element_present("errorExplanation").should be_true
 end
 
 Then /^the data is reported to billing system$/ do
   ret = RestClient.get "http://localhost:3001/invoices/#{@agreement_id}.xml"
   ret.should match '.*ok.*'
 end
-
-Then /^ordering is not possible$/ do
-  @browser.is_element_present('//input[@disabled=""]').should be_true
-end
-
 
 def fill_obligatory_fields()
  fill_employer_data
